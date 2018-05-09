f828e4a13d630eefaa20b2bee2bd830cc9bae489
xx <xx@xx.com>
2018-05-09 10:59:59 +0800

    aaaa

--- src/Test1.java
+++ src/Test1.java
@@ -14,6 +14,7 @@ public class Test1
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
+
         } catch (ClassNotFoundException e)
         {
             e.printStackTrace();
